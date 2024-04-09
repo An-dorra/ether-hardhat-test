d992fb712fe7f429f28246ab580d914666ed247a
洝小洝 <5479775+anzhimin@user.noreply.gitee.com>
2024-04-09 11:04:08 +0800

    The first commit

---
+++ deploy.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const { RPC_URL, PRIVATE_KEY } = process.env;
+const ethers = require("ethers");
+const fs = require("fs");
+const path = require("path");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+  const abi = fs.readFileSync(
+    path.resolve(__dirname, "./SimpleStorage_sol_SimpleStorage.abi"),
+    "utf8"
+  );
+  const binary = fs.readFileSync(
+    path.resolve(__dirname, "./SimpleStorage_sol_SimpleStorage.bin"),
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  // const transactionReceipt = await contract.deployTransaction.wait(1); //交易回执
+  const currFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currFavoriteNumber.toString()}`);
+
+  const transactionResponse = await contract.store("7");
+  await transactionResponse.wait(1);
+  const newFavoriteNumber = await contract.retrieve();
+  console.log(`Update Favorite Number: ${newFavoriteNumber}`);
+}
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
