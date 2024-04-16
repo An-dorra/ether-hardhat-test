cabcec5ea8c2c9828416ebb651df66df4246c589
洝小洝 <5479775+anzhimin@user.noreply.gitee.com>
2024-04-16 17:00:44 +0800

    添加测试网络，并部署在sepolia测试网

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+
+require("dotenv").config();
+
+require("./task/blonk_number");
+const { PRIVATE_KEY, RPC_URL, VERIFY_APP_KEYS } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.8",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: VERIFY_APP_KEYS,
+  },
 };
