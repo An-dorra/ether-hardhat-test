373b5147430dab6a857ccac1bffb4b9b38df9cf0
洝小洝 <5479775+anzhimin@user.noreply.gitee.com>
2024-04-16 17:57:13 +0800

    2、编写测试脚本

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,18 +3,22 @@ require("@nomicfoundation/hardhat-verify");
 
 require("dotenv").config();
 
-require("./task/blonk_number");
+require("./task/block_number");
 const { PRIVATE_KEY, RPC_URL, VERIFY_APP_KEYS } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
-  defaultNetwork: "sepolia",
+  defaultNetwork: "hardhat",
   networks: {
     sepolia: {
       url: RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
     },
   },
   etherscan: {
