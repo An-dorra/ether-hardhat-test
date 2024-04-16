9554394fd37861bffc7485e2c6e7aec1b783048f
洝小洝 <5479775+anzhimin@user.noreply.gitee.com>
2024-04-16 17:57:13 +0800

    1、添加loaclhost本地节点 2、编写测试脚本

--- task/block_number.js
+++ task/block_number.js
@@ -1,8 +1,8 @@
 const { task } = require("hardhat/config");
 
-task("blonk-number", "Prints the blonk number", async (taskArgs, hre) => {
+task("block-number", "Prints the block number", async (taskArgs, hre) => {
   const blockNumber = await hre.ethers.provider.getBlockNumber();
-  console.log("current blonk number:" + blockNumber);
+  console.log("current block number:" + blockNumber);
 });
 
 module.exports = {};
